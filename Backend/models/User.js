03d79b55a3e4193aa055edd9de4772a0bcd17a97
areeja48 <honor12pakistan@gmail.com>
2025-01-20 08:09:17 +0500

    update

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 //const Result = require("./Result");
 const User = sequelize.define('user', {
-  id: {type: DataTypes.INTEGER,allowNull: false,primaryKey: true,autoIncrement: true,},
+  id: {type: DataTypes.INTEGER,unique: true,primaryKey: true},
   username: { type: DataTypes.STRING, allowNull: false, unique: true },
   email: { type: DataTypes.STRING, allowNull: true, unique: true },  
   password: { type: DataTypes.STRING, allowNull: false },
