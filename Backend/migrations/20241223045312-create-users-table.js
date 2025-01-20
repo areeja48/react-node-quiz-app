fd28d05e606ffcba52c65978e3843ea2208d4321
areeja48 <honor12pakistan@gmail.com>
2025-01-20 07:45:58 +0500

    update

--- Backend/migrations/20241223045312-create-users-table.js
+++ Backend/migrations/20241223045312-create-users-table.js
@@ -4,8 +4,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('users', {
       id: {
-        allowNull: false,
-        autoIncrement: true,
+        
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
